e0d4feb69222963b8ca4583230d7ea81ac9c3288
haleh akrami <akrami@masal.usc.edu>
2018-09-10 11:39:06 -0700

    add logcosh

--- src/L1norm_decoder3.py
+++ src/L1norm_decoder3.py
@@ -8,10 +8,11 @@ x_test = x_test.astype('float32') / 255.
 x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
 x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
 
-noise_factor = 0.5
+noise_factor = 2
+x_test_noisy=x_test.copy()
 x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
-x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
-
+#x_test_noisy[0,8,15:20,0] = x_test[0,8,15:20,0] + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test[0,8,15:20,0].shape) 
+x_test_noisy[0,4,15:20,0] = 1
 x_train_noisy = np.clip(x_train_noisy, 0., 1.)
 x_test_noisy = np.clip(x_test_noisy, 0., 1.)
 
@@ -46,12 +47,12 @@ x = UpSampling2D((2, 2))(x)
 decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
 autoencoder = Model(input_img, decoded)
-autoencoder.compile(optimizer='adadelta', loss='mean_absolute_error')
+autoencoder.compile(optimizer='adadelta', loss='logcosh')
 
 from keras.callbacks import TensorBoard
 
 autoencoder.fit(x_train, x_train,
-                epochs=50,
+                epochs=2,
                 batch_size=128,
                 shuffle=True,
                 validation_data=(x_test, x_test),
@@ -67,14 +68,20 @@ n = 10
 plt.figure(figsize=(20, 4))
 for i in range(n):
     # display original
-    ax = plt.subplot(2, n, i+1)
+    ax = plt.subplot(3, n, i+1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    ax = plt.subplot(3, n, i+n+1)
     plt.imshow(x_test_noisy[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
     # display reconstruction
-    ax = plt.subplot(2, n, i + n+1)
+    ax = plt.subplot(3, n, i + 2*n+1)
     plt.imshow(decoded_imgs[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
