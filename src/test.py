10df4ab4640bbe1b0739aa5d4c3745ca39ceb47b
haleh akrami <akrami@masal.usc.edu>
2018-09-09 23:14:48 -0700

    compare in and decoder out

--- src/test.py
+++ src/test.py
@@ -57,26 +57,6 @@ autoencoder.fit(x_train, x_train,
                 validation_data=(x_test, x_test),
                callbacks=[TensorBoard(log_dir='/tmp/tb', histogram_freq=0, write_graph=False)])
 
-               decoded_imgs = autoencoder.predict(x_test)
-
-n = 10
-plt.figure(figsize=(20, 4))
-for i in range(n):
-    # display original
-    ax = plt.subplot(2, n, i+1)
-    plt.imshow(x_test[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-    # display reconstruction
-    ax = plt.subplot(2, n, i + n+1)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
-
 
 
 
