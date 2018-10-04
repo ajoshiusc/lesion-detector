3a9e4ca6f03868f1dee18835f0201d1fffff979e
Anand A Joshi <ajoshi@masal.usc.edu>
2018-10-04 16:59:13 -0700

    update

--- src/SpyderNet/datautils.py
+++ src/SpyderNet/datautils.py
@@ -7,20 +7,25 @@ import numpy as np
 def read_data(study_dir, nsub, npatch_perslice):
     dirlist = glob.glob(study_dir + '/TBI*')
     for subj in dirlist:
-        t1file = os.path.join(subj,'T1.nii.gz')
-        t2file = os.path.join(subj,'T2.nii.gz')
-        fl = os.path.join(subj,'FLAIR.nii.gz')
+        t1file = os.path.join(subj, 'T1.nii.gz')
+        t2file = os.path.join(subj, 'T2.nii.gz')
+        fl = os.path.join(subj, 'FLAIR.nii.gz')
 
+        if not (os.path.isfile(t1file) and os.path.isfile(t2file)
+                and os.path.isfile(fl)):
+            continue
+
+        # Read the three images
         t1 = nl.image.load_img(t1file).get_data()
         t2 = nl.image.load_img(t2file).get_data()
         flair = nl.image.load_img(fl).get_data()
 
-        imgs = np.stack((t1,t2,flair), axis = 3)
+        imgs = np.stack((t1, t2, flair), axis=3)
 
         # Read coronal slices
 
-#        create image
+        #        create image
 
-#        create random patches
+        #        create random patches
 
-        return patch_data # npatch x width x height x channels
+        return patch_data  # npatch x width x height x channels
