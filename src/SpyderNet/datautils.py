2846e35404ae254a742030dd385f4614a190b730
Anand A Joshi <ajoshi@masal.usc.edu>
2018-10-04 16:55:29 -0700

    update

--- src/SpyderNet/datautils.py
+++ src/SpyderNet/datautils.py
@@ -5,15 +5,15 @@ import numpy as np
 
 
 def read_data(study_dir, nsub, npatch_perslice):
-    dirlist = glob.glob(study_dir)
+    dirlist = glob.glob(study_dir + '/TBI*')
     for subj in dirlist:
         t1file = os.path.join(subj,'T1.nii.gz')
         t2file = os.path.join(subj,'T2.nii.gz')
         fl = os.path.join(subj,'FLAIR.nii.gz')
 
-        t1 = nl.load(t1file).get_data()
-        t2 = nl.load(t2file).get_data()
-        flair = nl.load(flfile).get_data()
+        t1 = nl.image.load_img(t1file).get_data()
+        t2 = nl.image.load_img(t2file).get_data()
+        flair = nl.image.load_img(fl).get_data()
 
         imgs = np.stack((t1,t2,flair), axis = 3)
 
--- src/SpyderNet/main_script.py
+++ src/SpyderNet/main_script.py
@@ -7,7 +7,7 @@ def main():
     """ Main script that calls the functions objects"""
     data_dir = '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot'
 
-    train_data = read_data(data_dir, nsub=5, npatches_perslice=16)
+    train_data = read_data(data_dir, nsub=5, npatch_perslice=16)
 
     model = train_model(data_dir=train_data_dir, csv_file=csv_file)
 
