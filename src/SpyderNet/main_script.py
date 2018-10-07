dfe16fd5fb80b878690e2d770cc880ddf2f0a082
Anand A Joshi <ajoshi@masal.usc.edu>
2018-10-07 13:14:21 -0700

    update

--- src/SpyderNet/main_script.py
+++ src/SpyderNet/main_script.py
@@ -9,27 +9,28 @@ def main():
     data_dir = '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot'
 
     data = read_data(
-        study_dir=data_dir, nsub=5, psize=[32, 32], npatch_perslice=256)
+        study_dir=data_dir, nsub=5, psize=[128, 128], npatch_perslice=32)
 
-    train_data = data #[0:-5, :, :, :]
+    train_data = data  #[0:-5, :, :, :]
     model = train_model(train_data)
 
-    test_data = data[90:95, :, :, :]
+    test_data = data[690:695, :, :, :]
     I, pred = mod_indep_rep(model, test_data)
 
     plt.figure()
     for j in range(5):
-        plt.subplot(2, 5, j + 1)
-        plt.imshow(I[j, :, :, :].squeeze())
-        plt.subplot(2, 5, 5 + j + 1)
-        plt.imshow(test_data[j, :, :, 0].squeeze())
-        plt.subplot(2, 5, 10 + j + 1)
-        plt.imshow(pred[j, :, :, 0].squeeze())
+        plt.subplot(3, 5, j + 1)
+        plt.imshow(I[j, :, :, :].squeeze(), cmap='gray')
+        plt.subplot(3, 5, 5 + j + 1)
+        plt.imshow(test_data[j, :, :, 0].squeeze(), cmap='gray')
+        plt.subplot(3, 5, 10 + j + 1)
+        plt.imshow(pred[0][j, :, :, 0].squeeze(), cmap='gray')
 
     plt.show()
 
     print(test_data)
     print(I)
+
 
 #    for j in range(5):
 #        plt.matshow(I[j,:,:,:].squeeze())
