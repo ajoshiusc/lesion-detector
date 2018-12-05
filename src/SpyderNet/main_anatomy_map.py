d2c462c28a3f44bd8dd2e4cf4ad30c14c5d6b626
haleh akrami <akrami@masal.usc.edu>
2018-12-04 16:04:02 -0800

    edit

--- src/SpyderNet/main_anatomy_map.py
+++ src/SpyderNet/main_anatomy_map.py
@@ -19,7 +19,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import pyplot as plt
-from spydernet import train_model, mod_indep_rep_vol, mod_indep_rep_vol
+from spydernet import train_model, mod_indep_rep_vol, mod_indep_rep_vol_stp
 from spydernet import train_model, mod_indep_rep
 #from tensorflow.python.client import device_lib
 """ Main script that calls the functions objects"""
@@ -32,86 +32,22 @@ with open(tbi_done_list) as f:
 # Get the list of subjects that are correctly registered
 tbidoneIds = [l.strip('\n\r') for l in tbidoneIds]
 
-data = read_data(
-    study_dir=data_dir,
-    subids=tbidoneIds,
-    nsub=10,
-    psize=[128, 128],
-    npatch_perslice=16)
+#data = read_data(
+#    study_dir=data_dir,
+#    subids=tbidoneIds,
+#    nsub=10,
+#    psize=[64, 64],
+#    npatch_perslice=16)
 
-np.savez('tp_data.npz', data=data)
+#np.savez('tp_data.npz', data=data)
 
-train_data = data  #[0:-5, :, :, :]
+#train_data = data  #[0:-5, :, :, :]
 
-model1 = train_model(train_data)
+#model1 = train_model(train_data)
 
-model1.save('tp_model_softmax.h5')
+#model1.save('tp_model_softmax.h5')
 
-test_data = data[90:95, :, :, :]
-
-model = load_model('tp_model_softmax.h5')
-
-I, pred = mod_indep_rep(model, test_data)
-
-plt.figure()
-for j in range(5):
-    plt.subplot(3, 5, j + 1)
-    plt.imshow(I[j, :, :, :].squeeze(), cmap='gray')
-    plt.subplot(3, 5, 5 + j + 1)
-    plt.imshow(test_data[j, :, :, 0].squeeze(), cmap='gray')
-    plt.subplot(3, 5, 10 + j + 1)
-    plt.imshow(pred[0][j, :, :, 0].squeeze(), cmap='gray')
-
-plt.show()
-
-print(test_data)
-print(I)
-
-#for j in range(5):
-#plt.matshow(I[j,:,:,:].squeeze())
-
-#plt.show()
-
-# In[2]:
-""" Main script that calls the functions objects"""
-
-model = load_model('tp_model_softmax.h5')
-
-d = np.load('tp_data.npz')
-
-data = d['data']
-test_data = data[190:195, :, :, :]
-
-# In[4]:
-
-plt.rcParams["axes.grid"] = False
-test_data = data[5000:5005, :, :, :]
-
-fig = plt.figure(figsize=(18, 16), dpi=100, facecolor='none', edgecolor='none')
-
-td = test_data.copy()
-
-I = mod_indep_rep_vol(model, td, 128)
-
-I.shape
-#fig=plt.figure(figsize=(20, 20))
-pred = test_data
-
-for j in range(5):
-    plt.subplot(5, 5, j + 1)
-    plt.imshow(I[j, :, :], cmap='gray')
-
-    plt.subplot(5, 5, 5 + j + 1)
-    plt.imshow(test_data[j, :, :, 0].squeeze(), cmap='gray')
-
-    plt.subplot(5, 5, 10 + j + 1)
-    plt.imshow(test_data[j, :, :, 1].squeeze(), cmap='gray')
-
-    plt.subplot(5, 5, 15 + j + 1)
-    plt.imshow(test_data[j, :, :, 2].squeeze(), cmap='gray')
-
-plt.show()
-
+model = load_model('/big_disk/ajoshi/coding_ground/lesion-detector/src/SpyderNet/tp_model_softmax.h5')
 t1 = ni.load_img(
     '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot/TBI_INVBB041DZW/T1.nii.gz'
 ).get_data()
@@ -132,15 +68,12 @@ t2 = np.float32(t2) / p
 
 p = np.percentile(np.ravel(flair), 95)  #normalize to 95 percentile
 flair = np.float32(flair) / p
-
 dat = np.stack((t1, t2, flair), axis=3)
-
-#dat=np.stack((t1.get_data(),t2.get_data(),flair.get_data()),axis=-1)
 
 print(dat.shape)
 dat = np.float32(dat)
 td = dat.copy()
-I2 = mod_indep_rep_vol(model, td, 128)
+I2 = mod_indep_rep_vol_stp(model, td, 128)
 
 fig = plt.figure(figsize=(20, 20))
 for j in range(5):
@@ -160,9 +93,8 @@ plt.show()
 
 print(I2.shape)
 
-img = ni.new_img_like(t1o, I2[:, :, :, 0])
-img.to_filename('/big_diak/ajoshi/fitbir/preproc/tracktbi_pilot/out.nii.gz')
+img = ni.new_img_like(t1o, I2)
+img.to_filename('/big_disk/ajoshi/out.nii.gz')
 
 # In[8]:
 
-I[1].shape
