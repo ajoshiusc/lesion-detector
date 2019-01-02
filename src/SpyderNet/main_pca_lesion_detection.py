5d64df092ec1b86736933dbe70b16ee03b8b2221
Anand A Joshi <ajoshi@masal.usc.edu>
2019-01-02 13:41:04 -0800

    update

--- src/SpyderNet/main_pca_lesion_detection.py
+++ src/SpyderNet/main_pca_lesion_detection.py
@@ -10,8 +10,8 @@ import numpy as np
 from keras.losses import mse
 import nilearn.image as ni
 
-data_dir = '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot'
-tbi_done_list = '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot_done.txt'
+data_dir = '/big_disk/ajoshi/fitbir/preproc/maryland_rao_v1'
+tbi_done_list = '/big_disk/ajoshi/fitbir/preproc/maryland_rao_v1_done.txt'
 
 with open(tbi_done_list) as f:
     tbidoneIds = f.readlines()
@@ -26,41 +26,57 @@ data = read_data(
     psize=[64, 64],
     npatch_perslice=16)
 
-model1 = pca_ae([64, 64, 3], 2*2*512)
+model1 = pca_ae([64, 64, 3], 2 * 2 * 512)
 model1.compile(optimizer='adamax', loss='mse')
 
 model1.fit(
-    x=data, y=data, shuffle=True, validation_split=.2, batch_size=32, epochs=100)
+    x=data,
+    y=data,
+    shuffle=True,
+    validation_split=.2,
+    batch_size=32,
+    epochs=100)
+
+model1.save('model1.h5')    
 
 data2 = model1.predict(data)
 
 print(np.mean((data2.flatten() - data.flatten())**2))
 
 t1 = ni.load_img(
-    '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot/TBI_INVBB041DZW/T1mni.nii.gz'
+    '/big_disk/ajoshi/fitbir/preproc/maryland_rao_v1/TBI_INVFK149AKJ/T1mni.nii.gz'
 ).get_data()
 t2 = ni.load_img(
-    '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot/TBI_INVBB041DZW/T2mni.nii.gz'
+    '/big_disk/ajoshi/fitbir/preproc/maryland_rao_v1maryland_rao_v1/TBI_INVFK149AKJ/T2mni.nii.gz'
 ).get_data()
 flair = ni.load_img(
-    '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot/TBI_INVBB041DZW/FLAIRmni.nii.gz'
+    '/big_disk/ajoshi/fitbir/preproc/maryland_rao_v1/TBI_INVFK149AKJ/FLAIRmni.nii.gz'
 ).get_data()
 t1o = ni.load_img(
-    '/big_disk/ajoshi/fitbir/preproc/tracktbi_pilot/TBI_INVBB041DZW/T1mni.nii.gz'
+    '/big_disk/ajoshi/fitbir/preproc/maryland_rao_v1/TBI_INVFK149AKJ/T1mni.nii.gz'
 )
 
 dat = np.stack((t1, t2, flair), axis=3)
 
 print(dat.shape)
 dat = np.float32(dat)
-td = dat.copy()
 
 #build_pca_autoencoder(model1, td, [64, 64, 3], step_size=1)
 out_vol = slice2vol_pred(model1.predict, dat, 64, step_size=1)
-
-t1=ni.new_img_like(t1o,out_vol[:,:,:,0])
+#%%
+t1 = ni.new_img_like(t1o, out_vol[:, :, :, 0])
 t1.to_filename('rec_t1.nii.gz')
 
+t2 = ni.new_img_like(t1o, out_vol[:, :, :, 1])
+t2.to_filename('rec_t2.nii.gz')
+
+flair = ni.new_img_like(t1o, out_vol[:, :, :, 2])
+flair.to_filename('rec_flair.nii.gz')
+
+err = ni.new_img_like(t1o, np.mean((out_vol - dat)**2, axis=3))
+err.to_filename('rec_err.nii.gz')
+
+np.savez('lesion_det.npz', out_vol=out_vol, dat=dat)
 print('vol created')
 print(out_vol.shape)
 print('done')
